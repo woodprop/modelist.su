65e87547941c7ac96dd8e72577cdb39a835a2d78
Konstantin <woodprop@yandex.ru>
2019-06-25 17:07:01 +0300

    Tag model addded

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,6 @@ from .views import *
 urlpatterns = [
     path('', post_list, name='post_list_url'),
     path('post/<str:slug>', post_detail, name='post_detail_url'),
+    path('tags/', tag_list, name='tag_list_url'),
+    path('tag/<slug>', tag_detail, name='tag_detail_url'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Post
+from .models import Post, Tag
 
 
 def post_list(request):
@@ -10,3 +10,13 @@ def post_list(request):
 def post_detail(request, slug):
     post = Post.objects.get(slug__iexact=slug)
     return render(request, 'blog/post_detail.html', context={'post': post})
+
+
+def tag_list(request):
+    tags = Tag.objects.all()
+    return render(request, 'blog/tag_list.html', context={'tags': tags})
+
+
+def tag_detail(request, slug):
+    tag = Tag.objects.get(slug__iexact=slug)
+    return render(request, 'blog/tag_detail.html', context={'tag': tag})
