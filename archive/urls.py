68d115b21d85628839e750246981fa5f30082b39
Konstantin <woodprop@yandex.ru>
2019-07-18 16:53:26 +0300

    Добавлен размер файла на странице чертежа

--- archive/urls.py
+++ archive/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', hello),
+    # path('', hello),
     path('plans/', PlansList.as_view(), name='plans_list_url'),
     path('plans/upload/', PlanUpload.as_view(), name='plan_upload_url'),
     path('plans/<slug>/', PlanDetail.as_view(), name='plan_detail_url'),
--- archive/views.py
+++ archive/views.py
@@ -1,3 +1,4 @@
+import os
 from django.shortcuts import render
 from django.shortcuts import redirect
 from django.core.exceptions import PermissionDenied
@@ -7,8 +8,10 @@ from .forms import *
 from django.http import HttpResponse
 
 
-def hello(request):
-    return HttpResponse('<h1>Hello from archive</h1>')
+def get_file_size(file):
+    byte_size = os.path.getsize('media/' + str(file))
+    mb_size = round(byte_size / 1024 / 1024, 2)
+    return mb_size
 
 
 class PlansList(View):
@@ -20,6 +23,7 @@ class PlansList(View):
 class PlanDetail(View):
     def get(self, request, slug):
         plan = Plan.objects.get(slug__iexact=slug)
+        plan.file_size = str(get_file_size(plan.file)) + ' MB'
         return render(request, 'archive/plan_detail.html', context={'plan': plan})
 
 
