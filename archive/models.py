836b22b859ae187f4b1f4481acfd9c70c427bf9b
Konstantin <woodprop@yandex.ru>
2019-07-18 20:47:34 +0300

    Добавлена модель тегов для чертежей, карточка чертежа вынесена в include-шаблон

--- archive/models.py
+++ archive/models.py
@@ -4,13 +4,14 @@ from uuslug import uuslug
 
 
 class Plan(models.Model):
-    name = models.CharField(max_length=50, db_index=True)
+    name = models.CharField(max_length=100, db_index=True)
     slug = models.SlugField(max_length=150, unique=True)
     description = models.TextField(blank=True, db_index=True)
     author = models.CharField(max_length=50, blank=True)
     image = models.ImageField(upload_to='plans/images')
     file = models.FileField(upload_to='plans/files')
     date_pub = models.DateField(auto_now_add=True)
+    tags = models.ManyToManyField('Tag', related_name='plans', blank=True)
 
     def get_absolute_url(self):
         return reverse('plan_detail_url', kwargs={'slug': self.slug})
@@ -18,6 +19,19 @@ class Plan(models.Model):
     def save(self, *args, **kwargs):
         self.slug = uuslug(self.name, instance=self, start_no=2)
         super(Plan, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100, db_index=True)
+    slug = models.SlugField(max_length=150, unique=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = uuslug(self.name, instance=self, start_no=2)
+        super(Tag, self).save(*args, **kwargs)
+        # TODO Или удалить или реализовать форму добавления
 
     def __str__(self):
         return self.name
